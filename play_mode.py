93d170149e20728189dff4982408ae874a469fcc
dnwls1133 <117172889+dnwls1133@users.noreply.github.com>
2025-11-06 15:52:16 +0900

    소년과 좀비의 충돌 소년과 좀비의 충돌 그룹을 만들어주고 소년과 좀비가 충돌하였을때 게임이 중단되게 만들었습니다.

--- play_mode.py
+++ play_mode.py
@@ -23,22 +23,25 @@ def handle_events():
 
 def init():
     global boy
-
+    global zombies
     grass = Grass()
     game_world.add_object(grass, 0)
     game_world.add_collision_pair('grass:ball',grass,None)
     boy = Boy()
     game_world.add_object(boy, 1)
-
+    game_world.add_collision_pair('boy:zombie',boy,None)
     global balls
     balls = [Ball(random.randint(100, 1600 - 100), 60, 0) for _ in range(30)]
     game_world.add_objects(balls, 1)
 
     zombies = [Zombie() for _ in range(4)]
     game_world.add_objects(zombies, 1)
+
+
+
     for zombie in zombies:
         game_world.add_collision_pair('zombie:ball', zombie, None)
-
+        game_world.add_collision_pair('boy:zombie',None,zombie)
     game_world.add_collision_pair('boy:ball',boy,None)
     for ball in balls:
         game_world.add_collision_pair('boy:ball',None,ball)
